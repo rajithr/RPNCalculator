bbc7118800b923324a1817e55976e785bebdcf48
rajithr <r.rajith@gmail.com>
2015-06-19 14:40:39 +0530

    Create TestData.java

---
+++ TestData.java
@@ -0,0 +1,21 @@
+
+public class TestData {
+	private String input;
+	private double result;
+	
+	public TestData(String s, double d)
+	{
+		input = s;
+		result = d;
+	}
+	
+	public String getInput()
+	{
+		return input;
+	}
+	
+	public double getResult()
+	{
+		return result;
+	}
+}
